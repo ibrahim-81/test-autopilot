45919d8df982392e899fd9c4dbc534eaddf4fad4
ibrahim-81 <67221629+ibrahim-81@users.noreply.github.com>
2023-10-14 14:41:56 +0300

    Add files via upload

---
+++ AutoPilot2.cmd
@@ -0,0 +1,7 @@
+@echo off
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; New-Item -Type Directory -Path 'C:\HWID'; 
+Set-Location -Path 'C:\HWID'; $env:Path += ';
+C:\Program Files\WindowsPowerShell\Scripts'; 
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned; 
+Install-Script -Name Get-WindowsAutopilotInfo; 
+Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv }"
